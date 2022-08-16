fc213138b372fd8af13da11e636752a1b0a682c0
Muhammadjon <mjalolov1305@gmail.com>
2022-08-16 20:48:43 +0500

    modifiying songs

--- js/playList.js
+++ js/playList.js
@@ -6,17 +6,17 @@ const playList = [
     },
     {
         title: 'Senorita',
-        src: '../assets/sounds/Senorita.mp3',
+        src: './assets/sounds/Senorita.mp3',
         duration: '00:33'
     },
     {
         title: 'What Is Love',
-        src: '../assets/sounds/What Is Love.mp3',
+        src: './assets/sounds/What Is Love.mp3',
         duration: '00:33'
     },
     {
         title: 'Yummy',
-        src: '../assets/sounds/Yummy.mp3',
+        src: './assets/sounds/Yummy.mp3',
         duration: '00:43'
     }
 ]
