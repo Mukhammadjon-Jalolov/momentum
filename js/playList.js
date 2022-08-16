b0a79991b337c38fa7f68667f4b20162f6075986
Muhammadjon <mjalolov1305@gmail.com>
2022-08-16 20:23:12 +0500

    modifiying songs

--- js/playList.js
+++ js/playList.js
@@ -1,23 +1,23 @@
 const playList = [
     {
+        title: 'Blinding Lights',
+        src: './assets/sounds/Blinding Lights.mp3',
+        duration: '00:29'
+    },
+    {
+        title: 'Senorita',
+        src: '../assets/sounds/Senorita.mp3',
+        duration: '00:33'
+    },
+    {
         title: 'What Is Love',
-        src: './assets/sounds/What Is Love.mp3',
-        duration: '00:34'
+        src: '../assets/sounds/What Is Love.mp3',
+        duration: '00:33'
     },
     {
-        title: 'Summer Wind',
-        src: '../assets/sounds/Summer Wind.mp3',
-        duration: '03:50'
-    },
-    {
-        title: 'River Flows In You',
-        src: '../assets/sounds/River Flows In You.mp3',
-        duration: '03:50'
-    },
-    {
-        title: 'Aqua Caelestis',
-        src: '../assets/sounds/Aqua Caelestis.mp3',
-        duration: '03:50'
+        title: 'Yummy',
+        src: '../assets/sounds/Yummy.mp3',
+        duration: '00:43'
     }
 ]
 
